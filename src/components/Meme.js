a2200d1432a9ad8e98ae207cf77ab696acbc368a
ricky <ricky.chaggar@gmail.com>
2022-04-29 12:29:23 +0100

    Change Meme.js to use State object
    Add meme stste object to Meme.js to hold top and bottom text lines
    and backgroudn image. Modify component to use meme state content.
    
    Set up allMemeImages state variable for holding meme image jason.

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,37 +3,43 @@ import buttonImage from "../images/meme.png";
 import memesData from "../data/memesData.js";
 
 function Meme() {
-  const [memeImage, setMemeImage] = useState("");
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+
+  const [allMemeImages, setAllMemeImages] = useState({ memesData });
 
   function getMemeImage() {
     const memesArray = memesData.data.memes;
     const randomNumber = Math.floor(Math.random() * memesArray.length);
     const memeUrl = memesArray[randomNumber].url;
-    setMemeImage(memeUrl);
+    setMeme((prevValue) => {
+      return { ...prevValue, randomImage: memeUrl };
+    });
   }
 
   return (
-    <div className="meme">
-      <div action="" className="memeGenerator">
-        <input
-          type="text"
-          className="meme__input"
-          id="firstLine"
-          placeholder="Top Line"
-        />
-        <input
-          type="text"
-          className="meme__input"
-          id="secondLine"
-          placeholder="Bottom Line"
-        />
-        <button type="submit" className="meme__button" onClick={getMemeImage}>
-          Get a new meme image
-          <img src={buttonImage} alt="" className="button__image" />{" "}
-        </button>
+    <div action="" className="memeGenerator">
+      <input
+        type="text"
+        className="meme__input"
+        id="firstLine"
+        placeholder={meme.topText}
+      />
+      <input
+        type="text"
+        className="meme__input"
+        id="secondLine"
+        placeholder={meme.bottomText}
+      />
+      <button type="submit" className="meme__button" onClick={getMemeImage}>
+        Get a new meme image
+        <img src={buttonImage} alt="" className="button__image" />{" "}
+      </button>
 
-        <img src={memeImage} alt="Meme" className="meme__image" />
-      </div>
+      <img src={meme.randomImage} alt="Meme" className="meme__image" />
     </div>
   );
 }
