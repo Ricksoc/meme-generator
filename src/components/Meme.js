48bca6888f3e8acf8f5242a90834f83880d4ea98
ricky <ricky.chaggar@gmail.com>
2022-05-04 16:28:12 +0100

    Set meme text to be controlled by state
    Use the meme state object to control the meme text display via the
    handleChange function using the setMeme callback.

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -21,19 +21,34 @@ function Meme() {
     }));
   }
 
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
+  }
+  console.log(meme);
+
   return (
     <div action="" className="memeGenerator">
       <input
         type="text"
         className="meme__input"
         id="firstLine"
-        placeholder={meme.topText}
+        placeholder="Top Line"
+        name="topText"
+        value={meme.topText}
+        onChange={handleChange}
       />
       <input
         type="text"
         className="meme__input"
         id="secondLine"
-        placeholder={meme.bottomText}
+        placeholder="Bottom Line"
+        name="bottomText"
+        value={meme.bottomText}
+        onChange={handleChange}
       />
       <button type="submit" className="meme__button" onClick={getMemeImage}>
         Get a new meme image
@@ -42,8 +57,8 @@ function Meme() {
 
       <div className="meme">
         <img src={meme.randomImage} alt="Meme" className="meme__image" />
-        <h2 className="meme__text top">Top Text</h2>
-        <h2 className="meme__text bottom">Bottom Text</h2>
+        <h2 className="meme__text top">{meme.topText}</h2>
+        <h2 className="meme__text bottom">{meme.bottomText}</h2>
       </div>
     </div>
   );
